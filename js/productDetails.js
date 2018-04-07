2ea49db3f7fcf16709b4183674b844ce5616cec5
zhongRui_HE <418351897@qq.com>
2018-04-07 23:15:15 +0800

    2018-04-07 23.15

--- js/productDetails.js
+++ js/productDetails.js
@@ -1,10 +1,3 @@
-$(function () {
-    $('.btn-1>div').click(function () {
-        $(this).addClass('sel');
-        $(this).siblings('div').removeClass('sel');
-    })
-    $('.btn-2>div').click(function () {
-        $(this).addClass('sel');
-        $(this).siblings('div').removeClass('sel');
-    })
+var mySwiper1 = new Swiper('#swiper1',{
+
 })
