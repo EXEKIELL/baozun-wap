329904cfc0ffd16c5de66dffd22bebfc5ef7c262
zhongRui_HE <418351897@qq.com>
2018-04-11 15:11:11 +0800

    2018-04-11 15.11

--- js/public.js
+++ js/public.js
@@ -1,6 +1,43 @@
 $(function () {
-    $(function () {
-        $('.del1').prevAll().remove();
-        $('.del2').nextAll().remove();
+    $('.del1').prevAll().remove();
+    $('.del2').nextAll().remove();
+    $('.head2').click(function () {
+        $('body').css({
+            "overflow-y":"hidden"
+        });
+        $('.mask').show();
+        $('.navBar').css({
+           "-webkit-transform": "matrix(+1,0,0,1,0,0)",
+            "-moz-transform": "matrix(+1,0,0,1,0,0)",
+            "-ms-transform": "matrix(+1,0,0,1,0,0)",
+            "-o-transform": "matrix(+1,0,0,1,0,0)",
+            "transform": "matrix(+1,0,0,1,0,0)"
+        })
+    })
+    $('.mask').click(function () {
+        $('.navBar').css({
+            "-webkit-transform": "matrix(+1,0,0,1,-550,0)",
+            "-moz-transform": "matrix(+1,0,0,1,-550,0)",
+            "-ms-transform": "matrix(+1,0,0,1,-550,0)",
+            "-o-transform": "matrix(+1,0,0,1,-550,0)",
+            "transform": "matrix(+1,0,0,1,-550,0)"
+        })
+        $('body').css({
+            "overflow-y":"scroll"
+        })
+        $(this).hide();
+    })
+    $('.navBar a').click(function () {
+        $('.navBar').css({
+            "-webkit-transform": "matrix(+1,0,0,1,-550,0)",
+            "-moz-transform": "matrix(+1,0,0,1,-550,0)",
+            "-ms-transform": "matrix(+1,0,0,1,-550,0)",
+            "-o-transform": "matrix(+1,0,0,1,-550,0)",
+            "transform": "matrix(+1,0,0,1,-550,0)"
+        })
+        $('body').css({
+            "overflow-y":"scroll"
+        });
+        $('.mask').hide()
     })
 })
